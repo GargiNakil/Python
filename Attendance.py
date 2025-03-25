1b2405409c2e6364fc059bafcc19494486620e58
GargiNakil <garginakil@gmail.com>
2025-03-25 23:22:11 +0530

    Add files via upload

---
+++ Attendance.py
@@ -0,0 +1,25 @@
+import time
+
+
+attendance_file = "attendance.txt"
+
+def mark_attendance():
+    emp_id = input("Enter your Employee ID: ")  # Employee enters their ID
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")  # Get current date & time
+
+    
+    with open(attendance_file, "a") as file:
+        file.write(f"{timestamp} - Employee ID: {emp_id}\n")
+
+    print(f"Attendance marked for Employee ID: {emp_id} at {timestamp}")
+
+
+while True:
+    print("\n--- Employee Attendance System ---")
+    mark_attendance()
+
+    # Ask if another employee wants to mark attendance
+    more = input("Mark another attendance? (yes/no): ").strip().lower()
+    if more != "yes":
+        print("Exiting Attendance System.")
+        break
